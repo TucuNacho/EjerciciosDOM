24d130934feb083023f7d338132317a5a6c886ec
Nacho <barrojojoaquin2@gmail.com>
2025-05-07 20:32:48 -0300

    agregamos una funcion que cambia el titulo de una card

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+const cambiarTitulo = () => {
+    console.log("Hola Mundo");
+    const titulo = document.getElementById("titulo");
+    console.log(titulo);
+    titulo.textContent = "Hola Mundo desde JS";
+    titulo.classList.add("text-primary");
+    
+}
