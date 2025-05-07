ae25a8893f9ded218c6e6579295a8ecc8fd558ac
Nacho <barrojojoaquin2@gmail.com>
2025-05-07 20:57:48 -0300

    agregamos una funcion que cambia un boton

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,14 @@
 const cambiarTitulo = () => {
-    console.log("Hola Mundo");
-    const titulo = document.getElementById("titulo");
-    console.log(titulo);
-    titulo.textContent = "Hola Mundo desde JS";
-    titulo.classList.add("text-primary");
-    
-}
+  console.log("Hola Mundo");
+  const titulo = document.getElementById("titulo");
+  console.log(titulo);
+  titulo.textContent = "Hola Mundo desde JS";
+  titulo.classList.add("text-primary");
+};
+
+const cambiarTituloInnerHTML = () => {
+  console.log("hola mundo");
+};
+
+const btnCambiarTitulo = document.getElementsByClassName("btn-outline-warning");
+console.log(btnCambiarTitulo);
