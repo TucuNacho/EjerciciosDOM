b627a9015536567f0e639565ebcbb43d6369c75f
Nacho <barrojojoaquin2@gmail.com>
2025-05-07 23:57:58 -0300

    funcion para ocultar un parrafo

--- JS/app.js
+++ JS/app.js
@@ -12,3 +12,17 @@ const cambiarTituloInnerHTML = () => {
 
 const btnCambiarTitulo = document.getElementsByClassName("btn-outline-warning");
 console.log(btnCambiarTitulo);
+
+document.querySelector('.btn-outline-warning')
+console.log(btnCambiarTitulo)
+//agregar un manejador de eventos
+// btnCambiarTitulo.addEventListener('click', cambiarTituloInnerHTML)
+
+const ocultarParrafo = () => {
+    console.log("Hola Mundo desde ocultarParrafo");
+    const parrafos = document.getElementsByTagName("p")
+    const btnOcultar = document.querySelector(".btn-danger")
+    console.log(parrafos[2]);
+    parrafos[2].classList.add("d-none")
+    btnOcultar.textContent = "Mostrar parrafo"
+}
